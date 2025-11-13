e2e2bb07d85fb538c90ca3a9575e9c35620bd1ad
David-bosonic <david.eberle@bosonic.digital>
2025-11-13 11:43:31 -0300

    add Empty project

--- backend/TestHub.Api/Data/TestHubContext.cs
+++ backend/TestHub.Api/Data/TestHubContext.cs
@@ -8,8 +8,22 @@ namespace TestHub.Api.Data
         public TestHubContext(DbContextOptions<TestHubContext> options) : base(options)
         {
         }
+
         public DbSet<Project> Projects { get; set; }
         public DbSet<TestRun> TestRuns { get; set; }
         public DbSet<TestResult> TestResults { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Project>().HasData(
+                new Project
+                {
+                    Id = 1,
+                    Name = "Empty Project"
+                }
+            );
+        }
     }
 }
